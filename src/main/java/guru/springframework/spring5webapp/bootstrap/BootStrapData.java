27870f8d9799ee7ae8944eab3c20e594d90ea99a
saidakyuz <saidakyuz@gmail.com>
2020-04-13 12:49:54 +0200

    lecture 17 Error is solved

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,7 +25,6 @@ public class BootStrapData implements CommandLineRunner {
         Book ddd = new Book("Domain Drive Design", "433543245435");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
-
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
@@ -33,9 +32,15 @@ public class BootStrapData implements CommandLineRunner {
         Book noEJB = new Book("J'EE Development Without EJB", "759034578057");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
-
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+
+        Author nfk = new Author("Necip Fazıl", "Kısakürek");
+        Book ovb = new Book("O ve Ben", "3457890683");
+        nfk.getBooks().add(ovb);
+        ovb.getAuthors().add(nfk);
+        authorRepository.save(nfk);
+        bookRepository.save(ovb);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books = " + bookRepository.count());
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
-import sun.util.resources.Bundles;
+
 
 import javax.persistence.*;
 import java.util.HashSet;
